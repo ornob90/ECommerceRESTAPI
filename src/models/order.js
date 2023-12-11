acbbf94a238fb5330059038a16843219c17710f4
ornob90 <oornob49@gmail.com>
2023-12-11 22:12:52 +0600

    updating the order model

--- src/models/order.js
+++ src/models/order.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  buyer: {
+  seller: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,

406520c9b82820fef2fb0d42d7f6e18cd95cf710
ornob90 <oornob49@gmail.com>
2023-12-12 07:32:45 +0600

    setup login api

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,5 +1,6 @@
+const login = require("./login");
 const signup = require("./signup");
-
 module.exports = {
   signup,
+  login,
 };
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,8 +1,9 @@
-const { signup } = require("../../api/auth");
+const { signup, login } = require("../../api/auth");
 
 const router = require("express").Router();
 
 // POST
 router.post("/api/auth/register", signup);
+router.post("/api/auth/login", login);
 
 module.exports = router;

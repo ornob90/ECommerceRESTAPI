afcf0214925f5783ac037b224ecfe7621a2574e5
ornob90 <oornob49@gmail.com>
2023-12-12 08:26:27 +0600

    verify buyer and admin middleware to better security

---
+++ src/middlewares/verifyBuyer.js
@@ -0,0 +1,19 @@
+const User = require("../models/user");
+
+const verifyBuyer = async (req, res, next) => {
+  try {
+    const { userName } = req.user;
+
+    const { userType } = (await User.findOne({ userName })) || {};
+
+    if (!userType || userType !== "buyer") {
+      res.status(403).send({ message: "unauthorized access" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = verifyBuyer;
--- src/routes/buyer/index.js
+++ src/routes/buyer/index.js
@@ -5,18 +5,29 @@ const {
   cancelOrder,
 } = require("../../api/buyer");
 const updateProfile = require("../../api/user/updateProfile");
+const verifyBuyer = require("../../middlewares/verifyBuyer");
 const verifyToken = require("../../middlewares/verifyToken");
 
 const router = require("express").Router();
 
 // GET
-router.get("/api/buyer/list-of-sellers", getAllSellers);
+router.get(
+  "/api/buyer/list-of-sellers",
+  verifyToken,
+  verifyBuyer,
+  getAllSellers
+);
 router.get("/api/buyer/seller-catalog/:sellerId", getSellerCatalogs);
 
 // POST
 router.post("/api/buyer/seller-catalog/:sellerId", createOrder);
 
 // DELETE
-router.delete("/api/buyer/cancel-order/:orderId", verifyToken, cancelOrder);
+router.delete(
+  "/api/buyer/cancel-order/:orderId",
+  verifyToken,
+  verifyBuyer,
+  cancelOrder
+);
 
 module.exports = router;
